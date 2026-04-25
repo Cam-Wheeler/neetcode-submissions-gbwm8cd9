5b9ed0861125e37cde7869a3ac963678a23179d1
Cam Wheeler <98394754+Cam-Wheeler@users.noreply.github.com>
2026-04-25 12:36:46 +0100

    Add: longest-repeating-substring-with-replacement - submission-5

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-5.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        
+        res = 0
+        tracker = defaultdict(int)
+        head = 0
+
+        for tail in range(len(s)):
+            tracker[s[tail]] += 1
+            replacements = (tail - head) + 1 - max(tracker.values())
+            while replacements > k:
+                tracker[s[head]] -= 1
+                head += 1
+                replacements = (tail - head) + 1  - max(tracker.values())
+            res = max(res, tail - head + 1)
+        return res
+

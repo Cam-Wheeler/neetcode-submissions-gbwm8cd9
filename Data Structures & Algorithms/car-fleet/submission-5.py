f3a72dfd3384536c71fd63695faa1d9048e086d2
Cam Wheeler <98394754+Cam-Wheeler@users.noreply.github.com>
2026-04-27 18:25:21 +0100

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.py
@@ -0,0 +1,21 @@
+from math import ceil
+from collections import deque
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        
+        ttt = [(pos, (target - pos) / speed) for pos, speed in zip(position, speed)]
+        ttt.sort()
+        ttt = deque(ttt)
+        fleets = 1
+        pos, time_to_arrival = ttt.pop()
+        while ttt:
+            if time_to_arrival < ttt[-1][1]:
+                fleets += 1
+                pos, time_to_arrival = ttt.pop()
+            else:
+                ttt.pop()
+        
+        return fleets
+
+
+

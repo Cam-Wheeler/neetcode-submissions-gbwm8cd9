02b235bc1bd294f3f11550cde6d64486cf8b7080
Cam Wheeler <98394754+Cam-Wheeler@users.noreply.github.com>
2026-05-10 15:54:57 +0100

    Add: dijkstra - submission-0

---
+++ Data Structures & Algorithms/dijkstra/submission-0.py
@@ -0,0 +1,39 @@
+import heapq
+from collections import defaultdict
+
+class Solution:
+    def shortestPath(self, n: int, edges: List[List[int]], src: int) -> Dict[int, int]:
+        graph = defaultdict(list)
+        for source, target, weight in edges:
+            graph[source].append((weight, target))
+
+        distances = {node: float("inf") for node in range(n)}
+        prev = {node: None for node in range(n)}
+        visited = set()
+
+        distances[src] = 0
+
+        min_heap = [(0, src)]
+
+        while min_heap:
+
+            current_distance, current_node = heapq.heappop(min_heap)
+
+            if current_node in visited:
+                continue
+            visited.add(current_node)
+
+            for neighbour_distance, neighbour in graph[current_node]:
+
+                new_distance = current_distance + neighbour_distance
+
+                if new_distance < distances[neighbour]:
+                    distances[neighbour] = new_distance
+                    prev[neighbour] = current_node
+                    heapq.heappush(min_heap, (new_distance, neighbour)) 
+        
+        for node in distances:
+            if node not in visited:
+                distances[node] = -1
+        
+        return distances

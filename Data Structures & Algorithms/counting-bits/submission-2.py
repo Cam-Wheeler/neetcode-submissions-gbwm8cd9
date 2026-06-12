9cd2a84bdbf4f9c80409479f044e230957ff742f
Cam Wheeler <98394754+Cam-Wheeler@users.noreply.github.com>
2026-06-12 13:41:13 +0100

    Add: counting-bits - submission-2

---
+++ Data Structures & Algorithms/counting-bits/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        res = []
+
+        for num in range(n + 1):
+            count = 0
+            for i in range(32):
+                count += num & 1
+                num >>= 1
+            res.append(count)
+        return res

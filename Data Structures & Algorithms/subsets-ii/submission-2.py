d764d20f882e83f1a8979ad8abaa9c23015f8904
Cam Wheeler <98394754+Cam-Wheeler@users.noreply.github.com>
2026-07-02 08:18:23 +0100

    Add: subsets-ii - submission-2

---
+++ Data Structures & Algorithms/subsets-ii/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        cache = set()
+
+        def dfs(current, i):
+
+            if i == len(nums):
+                if tuple(current) not in cache:
+                    cache.add(tuple(current))
+                    res.append(current.copy())
+                return 
+            
+            current.append(nums[i])
+            dfs(current, i + 1)
+            current.pop()
+            dfs(current, i + 1)
+
+            return
+
+        nums.sort()
+        dfs([], 0)
+
+        return res
+
+
+

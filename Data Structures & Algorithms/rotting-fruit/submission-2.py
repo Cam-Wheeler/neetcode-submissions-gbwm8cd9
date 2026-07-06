3051fe0992d7aa56ed4900dec0c6dfa634224364
Cam Wheeler <98394754+Cam-Wheeler@users.noreply.github.com>
2026-07-06 06:32:31 +0100

    Add: rotting-fruit - submission-2

---
+++ Data Structures & Algorithms/rotting-fruit/submission-2.py
@@ -0,0 +1,43 @@
+from collections import deque
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        
+        ROWS, COLS = len(grid), len(grid[0])
+        visited = set()
+        bananas = set()
+        q = deque()
+
+        def enqueue(r, c):
+            if (r < 0 or c < 0 or r == ROWS or c == COLS or (r, c) in visited or grid[r][c] != 1):
+                return
+            visited.add((r, c))
+            q.append((r, c))
+            return
+        
+        for row in range(ROWS):
+            for col in range(COLS):
+                if grid[row][col] == 1:
+                    bananas.add((row, col))
+                if grid[row][col] == 2:
+                    bananas.add((row, col))
+                    q.append((row, col))
+        if not q and not bananas:
+            return 0 
+
+        time = -1 # Cause we need to knock off the rotten ones first! 
+        while q:
+            for i in range(len(q)):
+
+                r, c = q.popleft()
+                bananas.remove((r, c))
+                enqueue(r + 1, c)
+                enqueue(r - 1, c)
+                enqueue(r, c + 1)
+                enqueue(r, c - 1)
+            time += 1
+    
+        return -1 if bananas else time
+
+
+
+

f6e75b35525b7e759873396d41e65c0974c48440
Cam Wheeler <98394754+Cam-Wheeler@users.noreply.github.com>
2026-07-07 08:40:16 +0100

    Add: course-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-2.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj = {i: [] for i in range(numCourses)}
+        for pre_req, course in prerequisites:
+            adj[pre_req].append(course) # we are doing it backwards! 
+
+
+        res = []
+        visited = set()
+        # topological sort
+        def dfs(node, current_path):
+
+            if node in current_path:
+                return False
+
+            current_path.add(node)
+            for neighbour in adj[node]:
+                if neighbour not in visited:
+                    if not dfs(neighbour, current_path):
+                        return False
+            current_path.remove(node)
+            
+            visited.add(node)
+            res.append(node)
+
+            return True
+
+        for node in adj:
+            if node not in visited:
+                if not dfs(node, set()):
+                    return []
+        return res
+
+        
+
+

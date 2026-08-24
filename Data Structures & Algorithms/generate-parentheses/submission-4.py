23597451c99ac2bad2ed75a2f722c6fbe2b0c770
Cam Wheeler <98394754+Cam-Wheeler@users.noreply.github.com>
2026-08-24 07:58:35 +0100

    Add: generate-parentheses - submission-4

---
+++ Data Structures & Algorithms/generate-parentheses/submission-4.py
@@ -0,0 +1,31 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+
+        o_cnt = 1
+        c_cnt = 0
+        current = "("
+        res = []
+
+        def dfs(current, o_cnt, c_cnt):
+
+            if o_cnt > n:
+                return
+
+            if c_cnt > o_cnt:
+                return
+            
+            if o_cnt == c_cnt and o_cnt == n and c_cnt == n:
+                res.append(current)
+                return
+            
+            current_o = current + "("
+            dfs(current_o, o_cnt + 1, c_cnt)
+
+            current_c = current + ")"
+            dfs(current_c, o_cnt, c_cnt + 1)
+
+            return
+
+        dfs(current, o_cnt, c_cnt)
+        return res
+

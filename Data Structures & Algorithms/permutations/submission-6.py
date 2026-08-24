58975407ace2e042d0bb815a788d4f338db6253b
Cam Wheeler <98394754+Cam-Wheeler@users.noreply.github.com>
2026-08-24 05:42:12 +0100

    Add: permutations - submission-6

---
+++ Data Structures & Algorithms/permutations/submission-6.py
@@ -0,0 +1,25 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        used = [False] * len(nums)
+        max_depth = len(nums)
+        res = []
+
+        def dfs(depth, current):
+
+            if len(current) == len(nums):
+                res.append(current.copy())
+                return
+            
+            for idx in range(len(nums)): # O(n)
+                if not used[idx]:
+                    used[idx] = True
+                    current.append(nums[idx])
+                    dfs(depth + 1, current)
+                    current.pop()
+                    used[idx] = False
+            
+            return
+
+        dfs(0, [])
+
+        return res
